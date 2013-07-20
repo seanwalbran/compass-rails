257cd2cc9424f69889d94c66997d5d08b8f162a8
Sean Walbran <swalbran@socialcast.com>
2013-07-20 17:19:45 -0500

    remove access attempt at StaticCompiler under rails4 where it does not exist

--- lib/compass-rails/railties.rb
+++ lib/compass-rails/railties.rb
@@ -1,8 +1,10 @@
 if defined?(::Rails)
   if CompassRails.rails2?
     require "compass-rails/railties/2_3"
-  elsif CompassRails.rails31? || CompassRails.rails32? || CompassRails.rails4?
+  elsif CompassRails.rails31? || CompassRails.rails32?
     require "compass-rails/railties/3_1"
+  elsif CompassRails.rails4?
+    require "compass-rails/railties/4_0"
   elsif CompassRails.rails3?
     require "compass-rails/railties/3_0"
   else
